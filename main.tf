2d843d71246a282fcc48e755400afc6f50e1e499
xinzhao219 <53591193+xinzhao219@users.noreply.github.com>
2021-07-23 15:48:57 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
   
 terraform {
+  backend "s3" {
+    profile = "devops"
+    bucket  = "xin-terraform-state-dev"
+    key     = "terraform-artifactory/terraform.tfstate"
+    region  = "ap-southeast-2"
+  }
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
     artifactory = {
       source  = "registry.terraform.io/jfrog/artifactory"
       version = "2.2.7"
     }
   }
+}
+
+provider "aws" {
+  profile = "devops"
+  region  = "ap-southeast-2"
 }
